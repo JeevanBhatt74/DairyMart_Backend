fe59a425f249141f1bb5372e3e0ad645fa0491dc
JeevanBhatt74 <iamjeevanrkb@gmail.com>
2026-03-02 11:53:02 +0545

    chore: add or update auth.route.ts

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -11,6 +11,9 @@ const router = Router();
 // Wrap async methods to catch errors if not using an async wrapper middleware
 router.post("/register", (req, res) => authController.register(req, res));
 router.post("/login", (req, res) => authController.login(req, res));
+router.post("/forgot-password", (req, res) => authController.forgotPassword(req, res));
+router.post("/verify-otp", (req, res) => authController.verifyOTP(req, res));
+router.post("/reset-password", (req, res) => authController.resetPassword(req, res));
 
 // Upload Profile Picture Route
 router.post(
@@ -20,8 +23,14 @@ router.post(
   (req, res) => authController.uploadProfilePicture(req, res)
 );
 
-// Get Logged In User Profile (GET /api/auth/:id)
-// Get Logged In User Profile (GET /api/auth/:id)
+// Get Logged In User Profile
+router.get(
+  "/profile",
+  authenticateUser,
+  (req, res) => authController.getProfile(req, res)
+);
+
+// Get User Profile By ID (GET /api/v1/users/:id)
 router.get(
   "/:id",
   authenticateUser,
